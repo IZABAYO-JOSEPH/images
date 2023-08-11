326fc687278b8156ffe247271cd1225b7d183e4f
Joseph Izabayo <joseph.izabayo@studentambassadors.com>
2023-08-11 10:00:54 +0200

    consumed API with react-query and axios

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+const url =
+  "https://api.unsplash.com/search/photos?client_id=vXrvCkSuqB52EcnA8pKmv4XSu9jG0GrusFvpFama1To&query=monkey";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>Error...</h4>
+      </section>
+    );
+  }
 
-export default Gallery
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No images found.....</h4>
+      </section>
+    );
+  }
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          ></img>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Gallery;
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const SearchForm = () => {
+  const handleSubmit = (event)=>{
+    event.preventDefault()
+  }
   return (
-    <div>SearchForm</div>
+    <section>
+      <h1 className='title'>unsplash images</h1>
+       <form className='search-form'onSubmit={handleSubmit}>
+        <input type="text"
+        className='form-input search-input'
+        name='search'
+        placeholder='cat'
+        
+        />
+        <button type='submit' className='btn'>search</button>
+       </form>
+    
+    </section>
   )
 }
 
